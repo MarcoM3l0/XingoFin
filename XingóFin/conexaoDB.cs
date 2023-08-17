2f065b8dafae435b102c974a2b3e12786c86f353
Marco Melo <josemarcomelonascimento@outlook.com>
2023-08-17 19:24:03 -0300

    feat: Adicionada classe de conexão com o banco de dados

---
+++ XingóFin/conexaoDB.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XingóFin
+{
+    // Classe responsável por lidar com a conexão ao banco de dados
+    internal class conexaoDB
+    {
+
+        // String de conexão para o banco de dados
+        private string stringConexao = "SERVER=localhost; DATABASE=xingofin_db; UID=root; PWD=; PORT=;";
+
+        // Objeto de conexão que será utilizado para interagir com o banco de dados
+        public MySqlConnection conexao = null;
+
+        // Método para abrir a conexão com o banco de dados
+        public void AbrirConexao()
+        {
+            try
+            {
+                // Inicializa uma nova instância da conexão usando a string de conexão
+                conexao = new MySqlConnection(stringConexao);
+                conexao.Open();
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Problema ao abrir o servidor. Erro: " + error.Message);
+            }
+        }
+
+        // Método para fechar a conexão com o banco de dados
+        public void FecharConexao()
+        {
+            try
+            {
+                conexao = new MySqlConnection(stringConexao); 
+                conexao.Close();
+                conexao.Dispose();
+                conexao.ClearAllPoolsAsync();
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show("Problema ao abrir o servidor.Erro: " + error.Message);
+            }
+        }
+
+    }
+}
