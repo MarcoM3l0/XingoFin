24b7dd4dfb766aaf63008b49660d32b62b736519
Marco Melo <josemarcomelonascimento@outlook.com>
2023-08-20 18:29:56 -0300

    style: mudança de layout das bordas

--- XingóFin/FrmLogin.Designer.cs
+++ XingóFin/FrmLogin.Designer.cs
@@ -134,7 +134,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmLogin";
-            this.Opacity = 0.6D;
+            this.Opacity = 0.8D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
