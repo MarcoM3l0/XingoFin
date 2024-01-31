6909b5909cda618cadc59e42dc7d3bd2176d7fba
Marco Melo <josemarcomelonascimento@outlook.com>
2024-01-31 16:20:33 -0300

    modificações pequenas

---
+++ XingóFin/FrmGraficosAnalise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XingóFin
+{
+    public partial class FrmGraficosAnalise : Form
+    {
+        public FrmGraficosAnalise()
+        {
+            InitializeComponent();
+        }
+    }
+}
